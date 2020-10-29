1502b5977501d79f660aa6a670adbc3da9fa9f23
Alexander <alexanderespinozajesus@gmail.com>
2020-10-29 17:32:48 -0500

    Update ordenar-lista2.py
    Ordenar lista

--- ordenar-lista2.py
+++ ordenar-lista2.py
@@ -1,4 +1,18 @@
-#Ordenar lista
+#Ingresar lista
+n=int(input("Ingresa la cantidad de números: "))
+i=0
+lista=[]
+while i<n:
+    x=float(input("Ingresa número: "))
+    lista.append(x)
+    
+    if i>0:
+        if lista[i-1]<lista[i]:
+            print("Vas en Ascendente")
+
+        else:
+            print("Vas en Descendente")
+    i+=1
 
 #Ya no entiendo este algoritmo :(
 yn=input("¿Quieres ordenar la lista? Y/N:")
